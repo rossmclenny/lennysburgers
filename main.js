56a55f300471b61313d58921f078e9e49786ffef
rossmclenny <rossmclennan85@gmail.com>
2020-11-11 19:16:38 +0000

    modal working with func

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
-function check () {
+function changeColor()
+{    
+    document.getElementsByTagName("li").style.color=("pink");
+}
+
+function check() {
     var c = 0
     var w = 0
-    var answer1 = document.getElementsByName('q1');
-    var answer2 = document.getElementsByName('q2');
-    var answer3 = document.getElementsByName('q3');
-// question 1
-    for (var i = 0; i < answer1.length; i++) {
-        if (answer1[i].checked) 
-        {
-            if (answer1[i].value == "Super Mario World") {
+    var radios = document.getElementsByName('q1');
+    var radios2 = document.getElementsByName('q2');
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            if (radios[i].value == "Super Mario World") {
                 c++
             } else {
                 w++
-            } 
-        } 
-    } 
-// question 2
-    for (var i = 0; i < answer2.length; i++) {
-    if (answer2[i].checked) 
-    {
-        if (answer2[i].value == "Famicom") 
-            {
-            c++
-            } else
-            {
-             w++    
-            } 
-        } 
-    } 
-// question 3
- 
-    alert(`you got ${c} answers correct and ${w} answers wrong!`)
+            }
+        }
+    }
+    for (var i = 0; i < radios2.length; i++) {
+        if (radios2[i].checked) {
+            if (radios2[i].value == "f") {
+                c++
+            } else {
+                w++
+            }
+        }
+    }
+
+    // alert(`you got ${c} answers correct and ${w} answers wrong!`);
+
+    // SHOW MODAL
+    showModal(c, w);
+
 } //end of check function
+
+function showModal(c, w) {
+    var mod = document.getElementById('myModal');
+    mod.innerHTML = `you got ${c} answers correct and ${w} answers wrong!`;
+    mod.innerHTML += '<button onclick="hideModal()">Close</button>'
+    mod.setAttribute('style', 'display: block;');
+}
+
+function hideModal() {
+    var mod = document.getElementById('myModal');
+    mod.setAttribute('style', 'display: none;');
+}
